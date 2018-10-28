91b6ab308fd32083b7a6a706636fe5a27d6f542a
Pfoof <rotworm0@yahoo.pl>
2018-10-28 16:49:50 +0100

    Staty: dodano rekordy, indeksy

--- src/pg/ppabis/sbd2/Script.java
+++ src/pg/ppabis/sbd2/Script.java
@@ -95,6 +95,7 @@ public class Script {
                 break;
             case "stat":
             	System.out.println("Reorgs: "+Page.stat_reorg+"\nReads: "+Page.stat_reads+"\nWrites: "+Page.stat_writes);
+            	System.out.println("Records: "+Page.mainRecords+"\nOverflows: "+Page.overflowRecords+"\nIndexes/Pages: "+Index.indexes.length);
             	break;
             case "print":
                 Main.printDb(-1, -1, new int[]{-1, -1}, true);
